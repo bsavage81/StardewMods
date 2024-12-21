98ee681441952d8c214c7db5292a694f2edecbbd
BSavage81 <beau.j.savage@gmail.com>
2024-12-21 13:09:52 -0600

    Update

---
+++ DifferentHorseSkins/DifferentHorseSkins/IContentPatcherAPI.cs
@@ -0,0 +1,12 @@
+﻿using StardewModdingAPI;
+
+public interface IContentPatcherAPI
+{
+    /// <summary>
+    /// Register a token for use in Content Patcher patches.
+    /// </summary>
+    /// <param name="mod">The mod registering the token.</param>
+    /// <param name="name">The token name.</param>
+    /// <param name="values">A function which returns the current token values.</param>
+    void RegisterToken(IManifest mod, string name, Func<IEnumerable<string>> values);
+}
